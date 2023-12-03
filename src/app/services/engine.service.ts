af9c9d76a8e90361a26e737a620755481955e99e
slinghshot <simerpreet.singh40@gmail.com>
2023-12-03 14:15:59 -0800

    Update engine.service.ts

--- src/app/services/engine.service.ts
+++ src/app/services/engine.service.ts
@@ -10,7 +10,7 @@ import { InputError } from '../models/input-error';
 })
 export class EngineService {
   // private apiGetALL = 'http://127.0.0.1:3000/api/v1/meals';
-  private apiGetALL = 'https://api.mealplannit.com:3000/api/v1/meals';
+  private apiGetALL = 'https://api.mealplannit.com/api/v1/meals';
   ingredientsList: Array<string> = [];
   generatedIngredients: Array<any> = new Array<any>();
   measurements = [
